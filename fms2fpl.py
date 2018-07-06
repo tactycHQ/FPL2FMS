fe399916fdee20c31851db1e61c6203bee172e72
Anubhav <anubhav13@gmail.com>
2018-07-06 15:15:15 -0700

    Almost therE

--- fms2fpl.py
+++ fms2fpl.py
@@ -4,30 +4,40 @@ from tkinter import *
 from tkinter import filedialog
 import xml.etree.ElementTree as ET
 
-ident =[]
-type = []
-ccode =[]
-lat = []
-lon =[]
 
 def OpenFile():
     root = Tk();root.withdraw()
-    filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("fpl","*.fpl"),("all files","*.*")))
+    # filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("fpl","*.fpl"),("all files","*.*")))
+    filename = 'KORD.fpl'
     f = open(filename)
     txt = f.read()
     f.close()
     return txt
 
 def ParseXML():
+    ident = []
+    type = []
+    ccode = []
+    lat = []
+    lon = []
     tree = ET.ElementTree(ET.fromstring(OpenFile()))
     root = tree.getroot()
-    # print (root[1][1][1].tag)
-    # print(root[1][1][1].text)
-    print(root[1].tag.find('}')+1)
-    wpl = [wpt.text for wpt in tree.iter() if wpt.tag == 'type']
-    print(wpl)
+    nspc = root.tag[0:root.tag.find("}")+1]
+
+    for wpt in root[1].findall(nspc+"waypoint"):
+        ident.append(wpt.find(nspc+"identifier").text)
+        type.append(wpt.find(nspc + "type").text)
+        ccode.append(wpt.find(nspc + "country-code").text)
+        lat.append(wpt.find(nspc + "lat").text)
+        lon.append(wpt.find(nspc + "lon").text)
+    return ident,type,ccode,lat,lon
+
+def createFMS (ident,type,ccode,lat,lon):
+    header="I\n1100 Version"
+    out =""
+    print(header)
 
 
-
-
-ParseXML()
+#Runtime
+i,t,c,lt,ln = ParseXML()
+createFMS(i,t,c,lt,ln)
